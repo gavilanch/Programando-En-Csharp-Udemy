38aab4f0985657a8184b557e15b0251086f26a57
Felipe Gavilan <elmejor_887@hotmail.com>
2022-10-04 18:58:28 -0400

    Subo nuevo modulo

---
+++ CSharp 10/Modulo 1 - Introduccion/ResolviendoUnEjercicio/Program.cs
@@ -0,0 +1,4 @@
+﻿// Mensaje del profesor (Felipe Gavilán): Ve a la carpeta 'Codigo' para ver los ejemplos de este módulo.
+// Los ejercicios se encuentran en la carpeta 'Ejercicios'
+// Las soluciones de los ejercicios se encuentran en la carpeta 'Soluciones'
+Console.WriteLine("Hola, mundo");
