e905dfc113c4ed162e373715dc809be07f966211
Felipe Gavilán <gavilanch@hotmail.com>
2025-09-27 14:53:15 -0400

    Actualizacion

---
+++ CSharp 10/Modulo 6 - Clases, Structs y Records/Clases/ClaseConPropiedadesInmutables.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    internal class ClaseConPropiedadesInmutables
+    {
+        public int Propiedad1 { get; init; }
+        public string Propiedad2 { get; init; } = "";
+        public bool Propiedad3 { get; init; }
+    }
+}
---
+++ CSharp 10/Modulo 6 - Clases, Structs y Records/Clases/Codigo/PropiedadesInmutablesConInit.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases.Codigo
+{
+    internal class PropiedadesInmutablesConInit
+    {
+        internal class ClaseConPropiedadesInmutables
+        {
+            public int Propiedad1 { get; init; }
+            public string Propiedad2 { get; init; } = "";
+            public bool Propiedad3 { get; init; }
+        }
+
+        public void CodigoDelCurso()
+        {
+            var clase = new ClaseConPropiedadesInmutables()
+            {
+                Propiedad1 = 42,
+                Propiedad3 = true,
+                Propiedad2 = "Felipe"
+            };
+
+            //clase.Propiedad2 = "Roberto";
+        }
+    }
+}
